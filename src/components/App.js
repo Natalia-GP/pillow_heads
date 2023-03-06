8b2af78e802ce242a60cd00bb24264dfdebb05c5
Natalia-GP <nataliag86@yahoo.es>
2023-03-06 11:05:29 +0100

    react template

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import '../styles/App.scss';
+
+function App() {
+  // VARIABLES ESTADO
+
+  // USEEFFECT ?
+
+  // FUNCIONES HANDLER
+
+  // FUNCIONES Y VARIABLES QUE AYUDEN A RENDERIZAR HTML
+
+  // HTML EN EL RETURN
+
+  return (
+    <div className="App">
+      <h1 className="title">Rellename!</h1>
+
+      {/* Aquí va tu código HTML. */}
+    </div>
+  );
+}
+
+export default App;

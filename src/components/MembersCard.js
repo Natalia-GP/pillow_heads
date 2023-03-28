f1469e4a90893462901ec28bddc892f682c6e8d7
Calpurniax <sofia.gracia@gmail.com>
2023-03-28 13:37:25 +0200

    css members finish

--- src/components/MembersCard.js
+++ src/components/MembersCard.js
@@ -1,38 +1,37 @@
 import MemberLink from './MemberLink';
 const MembersCard = ({ eachPillow, index }) => {
   return (
-    <article className={`${eachPillow.class} members__article`} key={index}>
+    <article className={`members__article`} key={index}>
       <img
         className="members__article__img"
         src={eachPillow.photo}
         alt={eachPillow.name}
       />
-      <div className="members__article__div">
-        <h3 className="members__article__div__name">{eachPillow.name}</h3>
-        <p className="members__article__div__text">{eachPillow.description}</p>
-        <ul className="members__socials">
-          {eachPillow.twitter !== '' ? (
-            <MemberLink
-              link={eachPillow.twitter}
-              iClass={'fa-brands fa-twitter'}
-              dot={'withDot'}
-            />
-          ) : (
-            <li></li>
-          )}
+      <h3 className="members__article__div__name">{eachPillow.name}</h3>
+      <p className="members__article__div__text">{eachPillow.description}</p>
+      <ul className="members__socials">
+        {eachPillow.twitter !== '' ? (
           <MemberLink
-            link={eachPillow.linkedin}
-            iClass={'fa-brands fa-linkedin-in'}
+            link={eachPillow.twitter}
+            iClass={'fa-brands fa-twitter'}
             dot={'withDot'}
           />
-          <MemberLink
-            link={eachPillow.github}
-            iClass={'fa-brands fa-github-alt'}
-            dot={'withDot'}
-          />
-          <MemberLink link={eachPillow.email} iClass={'fa-solid fa-envelope'} />
-        </ul>
-      </div>
+        ) : (
+          <li></li>
+        )}
+        <MemberLink
+          link={eachPillow.linkedin}
+          iClass={'fa-brands fa-linkedin-in'}
+          dot={'withDot'}
+        />
+        <MemberLink
+          link={eachPillow.github}
+          iClass={'fa-brands fa-github-alt'}
+          dot={'withDot'}
+        />
+        <MemberLink link={eachPillow.email} iClass={'fa-solid fa-envelope'} />
+      </ul>
+
     </article>
   );
 };
